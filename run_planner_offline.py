3375cac0d9a15fff34b98f04952bcff575e8c7ac
tiejean <taijingchen99@gmail.com>
2024-11-21 07:33:40 -0600

    add generate_system_prompt_v2 (did not work well)

--- run_planner_offline.py
+++ run_planner_offline.py
@@ -4,8 +4,13 @@ from remembr.planners.remember_planner import ReMEmbRPlanner
 from remembr.memloaders.memloaders import *
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, required=True)
+    args = parser.parse_args()
     # memory = remember_demo(observations=["The video starts with a shot of a kitchen counter with various cooking ingredients and utensils scattered around. The person's hand is seen reaching for a bottle of olive oil and a jar of spices. The camera then pans to a white cabinet where the person takes out a frying pan and places it on the stove. The person then opens the refrigerator and takes out a bag of cauliflower. The person places the cauliflower on the frying pan and begins to cook it. The camera focuses on the cauliflower as it cooks, with the person occasionally stirring it. The video ends with the person taking the cooked cauliflower out of the frying pan."])
-    memory = remember_cobot(inpath="/home/tiejean/Workspace/RobotMem/data/captions/iphones/taijing_kitchen_VILA1.5-8b_5_secs.json")
+    # memory = remember_cobot(inpath="/home/tiejean/Workspace/RobotMem/data/captions/iphones/taijing_kitchen_VILA1.5-8b_3_secs.json")
+    memory = remember_cobot(inpath=args.data_path)
     agent = ReMEmbRPlanner(llm_type='gpt-4o')
     agent.set_memory(memory)
     while True:
