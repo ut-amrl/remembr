c3f79009e9f6e0d298c4b2cec8066a97f99760bb
TieJean <36142426+TieJean@users.noreply.github.com>
2025-01-01 20:58:15 -0600

    [Feature] Refactor planning problem (#5)
    * clean up chat_history
    
    * change max_agent_call_cnt to config
    
    * drafting
    
    * add objects
    
    * draft v0.1
    
    * add object-level planner
    
    * add order_plans_prompt

--- remembr/planners/remember_planner.py
+++ remembr/planners/remember_planner.py
@@ -59,11 +59,63 @@ def parse_json(string):
     parsed = re.search(r"```json(.*?)```", string, re.DOTALL| re.IGNORECASE).group(1).strip()
     return eval(parsed)
 
+def parse_response(response: str, special_keys: list = [], messages = None):
+    # let us parse and check the output is a dictionary. raise error otherwise
+    response = ''.join(response.content.splitlines())
+    
+    try:
+        if '```json' not in response:
+            # try parsing on its own since we cannot always trust llms
+            parsed = eval(response) 
+        else:
+            parsed = parse_json(response)
+            
+        for key in special_keys:
+            if key not in parsed.keys():
+                 raise ValueError("Generate call failed. Retrying...")
+            if type(parsed[key]) == str:
+                parsed[key] = eval(parsed[key])
+
+    except:
+        raise ValueError("Generate call failed. Retrying...")
+    return parsed
+
+def replace_messages(current: Sequence, new: Sequence):
+    """Custom update strategy to replace the previous value with the new one."""
+    return new
+
 class AgentState(TypedDict):
     # The add_messages function defines how an update should be processed
     # Default is to replace. add_messages says "append"
     messages: Annotated[Sequence[BaseMessage], add_messages]
+    context: Annotated[Sequence, replace_messages]
+    object_level_plans: Annotated[Sequence, replace_messages]
+    records: Annotated[Sequence, replace_messages] # TODO delete me
+    toolcalls: Annotated[Sequence, replace_messages] # TODO delete me
 
+def filter_retrieved_record(messages: list):
+    records = [msg.content for msg in filter(lambda x: isinstance(x, ToolMessage), messages)]
+    return sorted(list(set(records)))
+
+def filter_toolcalls(messages: list):
+    toolcalls = [msg for msg in filter(lambda x: isinstance(x, AIMessage) and len(x.tool_calls) > 0, messages)]
+    return toolcalls
+
+def from_objects_to(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "object_plans"
+    else:
+        return "objects_action"
+    
+def from_object_plans_to(state: AgentState):
+    object_plans = state["object_level_plans"]
+    for _, v in object_plans.items():
+        if not v["has_planned"]:
+            return "object_plans_action"
+        else:
+            return "order_plans" 
 
 # Define the function that determines whether to continue or not
 def from_agent_to(state: AgentState):
@@ -91,6 +143,10 @@ def try_except_continue(state, func):
 class ReMEmbRPlanner(Planner):
 
     def __init__(self, llm_type='gpt-4o', num_ctx=8192, temperature=0):
+        # TODO read in from some config file
+        self.config_max_objects_call_cnt = 3
+        self.config_max_object_plans_call_cnt = 1
+        self.config_max_agent_call_cnt = 3
 
         # Wrapper that handles everything
         llm = self.llm_selector(llm_type, temperature, num_ctx)
@@ -105,12 +161,15 @@ class ReMEmbRPlanner(Planner):
         self.embeddings = HuggingFaceEmbeddings(model_name='mixedbread-ai/mxbai-embed-large-v1')
 
         top_level_path = str(os.path.dirname(__file__)) + '/../'
-        self.agent_prompt = file_to_string(top_level_path+'prompts/planner/planner_system_prompt.txt')
-        self.generate_prompt = file_to_string(top_level_path+'prompts/planner/generate_system_prompt.txt')
-        self.agent_gen_only_prompt = file_to_string(top_level_path+'prompts/planner/planner_gen_system_prompt.txt')
-        self.critic_prompt = file_to_string(top_level_path+'prompts/planner/critic_system_prompt.txt')
+        self.objects_prompt = file_to_string(top_level_path+'prompts/planner/planner_objects_prompt.txt')
+        self.objects_terminate_prompt = file_to_string(top_level_path+'prompts/planner/planner_objects_terminate_prompt.txt')
+        self.object_plans_prompt = file_to_string(top_level_path+'prompts/planner/planner_object_plans_prompt.txt')
+        self.object_plans_terminate_prompt = file_to_string(top_level_path+'prompts/planner/planner_object_plans_terminate_prompt.txt')
+        self.order_plans_prompt = file_to_string(top_level_path+'prompts/planner/order_plans_prompt.txt')
 
         self.previous_tool_requests = "These are the tools I have previously used so far: \n"
+        self.objects_call_count = 0
+        self.object_plans_call_count = 0
         self.agent_call_count = 0
 
         self.chat_history = ChatMessageHistory()
@@ -144,10 +203,6 @@ class ReMEmbRPlanner(Planner):
         self.build_graph()
 
     def create_tools(self, memory):
-
-        template = "At time={{time}} seconds, the robot was at an average position of {{position}} with an average orientation of {{theta}} radians. "
-        template += "The robot saw the following: {{page_content}}"
-
 
         class TextRetrieverInput(BaseModel):
             x: str = Field(description="The query that will be searched by the vector similarity-based retriever.\
@@ -198,6 +253,138 @@ class ReMEmbRPlanner(Planner):
         self.tool_list = [self.retriever_tool, self.position_retriever_tool, self.time_retriever_tool]
         self.tool_definitions = [convert_to_openai_function(t) for t in self.tool_list]
 
+    def objects(self, state):
+        messages = state["messages"]
+        model = self.chat
+        if self.objects_call_count < self.config_max_objects_call_cnt:
+            model = model.bind_tools(tools=self.tool_definitions)
+            prompt = self.objects_prompt
+        else:
+            prompt = self.objects_terminate_prompt
+            self.objects_call_count = 0
+        
+        objects_prompt = ChatPromptTemplate.from_messages(
+            [
+                # ("system", prompt),
+                MessagesPlaceholder("chat_history"),
+                (("human"), self.previous_tool_requests),
+                ("ai", prompt),
+                ("human", "{question}"),
+            ]
+        )
+        model = objects_prompt | model
+        question = f"User-specified request: {messages[0]}"
+        # Convert all ToolMessages into AI Messages since Ollama cann't handle ToolMessage
+        if ('gpt-4' not in self.llm_type) and ('nim' not in self.llm_type):
+            for i in range(len(messages)):
+                if type(messages[i]) == ToolMessage:
+                    messages[i] = AIMessage(id=messages[i].id, content=messages[i].content) # ignore tool_call_id
+        response = model.invoke({"question": question, "chat_history": messages[:]})
+        
+        if response.tool_calls:
+            for tool_call in response.tool_calls:
+                if tool_call['name'] != "__conversational_response":
+                    args = re.sub("\{.*?\}", "", str(tool_call['args'])) # remove curly braces
+                    self.previous_tool_requests += f"I previously used the {tool_call['name']} tool with the arguments: {args}.\n"
+
+        self.objects_call_count += 1
+        
+        return {"messages": [response]}
+    
+    def object_plans(self, state):
+        messages = state["messages"]
+        object_plans = state["object_level_plans"]
+        context = state["context"]
+        if messages[-1].content and "objects" in messages[-1].content and "answer_reasoning" in messages[-1].content:
+            last_response = parse_response(messages[-1], special_keys=["objects"])
+            object_plans = {obj:{"has_planned":False, "plans":[]} for obj in last_response["objects"]}
+            context = last_response["answer_reasoning"]
+            
+        object = None
+        for k,v in object_plans.items():
+            if not v["has_planned"]:
+                object = k
+        if object is None:
+            return None # FIXME
+        
+        model = self.chat
+        if self.object_plans_call_count < self.config_max_object_plans_call_cnt:
+            model = model.bind_tools(tools=self.tool_definitions)
+            prompt = self.object_plans_prompt
+        else:
+            prompt = self.object_plans_terminate_prompt
+            object_plans[k]["has_planned"] = True
+            self.object_plans_call_count = 0
+            
+        object_plans_prompt = ChatPromptTemplate.from_messages(
+            [
+                MessagesPlaceholder("chat_history"),
+                (("human"), self.previous_tool_requests),
+                ("ai", prompt),
+                ("human", "{question}"),
+            ]
+        )
+        question = f"Context: {context}.\n Question: How can you retrieve {object}? Where are you likely to find this object? What actions should you take?"
+        model = object_plans_prompt | model
+        
+        # Convert all ToolMessages into AI Messages since Ollama cann't handle ToolMessage
+        if ('gpt-4' not in self.llm_type) and ('nim' not in self.llm_type):
+            for i in range(len(messages)):
+                if type(messages[i]) == ToolMessage:
+                    messages[i] = AIMessage(id=messages[i].id, content=messages[i].content) # ignore tool_call_id
+        retrieved_records = filter_retrieved_record(messages=messages)
+        response = model.invoke({"question": question, "chat_history": retrieved_records})
+        
+        if not response.tool_calls and response.content:
+            parsed_response = parse_response(response, special_keys=["plans"])
+            object_plans[object]["plans"] = parsed_response["plans"]
+            object_plans[object]["has_planned"] = True
+            
+        self.object_plans_call_count += 1
+            
+        import copy
+        return {"messages": [response], "context": context, "object_level_plans": copy.deepcopy(object_plans)}
+
+    def order(self, state):
+        object_plans = state["object_level_plans"]
+        context = state["context"]
+        model = self.chat
+        prompt = self.order_plans_prompt
+        order_plans_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("ai", prompt),
+                ("human", "{info}"),
+            ]
+        )
+        model = order_plans_prompt | model
+        plans = {k:v["plans"] for k,v in object_plans.items()}
+        import json; plans = json.dumps(plans)
+        info = f"Context: {context}\nAll possible steps:\n{plans}"
+        response = model.invoke({"info": info})
+        response = ''.join(response.content.splitlines())
+        
+        try:
+            if '```json' not in response:
+                # try parsing on its own since we cannot always trust llms
+                parsed = eval(response) 
+            else:
+                parsed = parse_json(response)
+            if 'plans' not in parsed.keys():
+                raise ValueError("Missing all the required keys during generate. Retrying...")
+            if type(parsed['plans']) == str:
+                parsed['plans'] = eval(parsed['plans'])
+        except:
+            raise ValueError("Generate call failed. Retrying...")
+        # TODO delete me
+        print(context)
+        for p in parsed["plans"]:
+            print(f"{p['plan']} at {p['position']}")
+        print()
+        for p in parsed["plans"]:
+            print(f"{p['plan']} at {p['position']}: {p['desc']}")
+        import pdb; pdb.set_trace()
+        return {"messages": [str(parsed)]}
+
     ### Nodes
     def agent(self, state):
         """
@@ -215,12 +402,12 @@ class ReMEmbRPlanner(Planner):
         model = self.chat
 
 
-        # limit to 5 tool calls.
-        if self.agent_call_count < 3:
+        # limit tool calls.
+        if self.agent_call_count < self.config_max_agent_call_cnt:
             model = model.bind_tools(tools=self.tool_definitions)
             prompt = self.agent_prompt
         else:
-            prompt = self.agent_gen_only_prompt
+            prompt = self.agent_terminate_prompt
 
 
         agent_prompt = ChatPromptTemplate.from_messages(
@@ -289,9 +476,10 @@ class ReMEmbRPlanner(Planner):
         )
 
         model = gen_prompt | self.chat
-        
-        response = model.invoke({"question": question, "chat_history": messages[1:]})
-        
+        retrieved_record = filter_retrieved_record(messages=messages)
+        # response = model.invoke({"question": question, "chat_history": messages[1:]})
+        response = model.invoke({"question": question, "chat_history": retrieved_record})
+
         # let us parse and check the output is a dictionary. raise error otherwise
         response = ''.join(response.content.splitlines())
         
@@ -326,30 +514,57 @@ class ReMEmbRPlanner(Planner):
 
         # Define a new graph
         workflow = StateGraph(AgentState)
-
-        # Define the nodes we will cycle between
-        workflow.add_node("agent", lambda state: try_except_continue(state, self.agent))  # agent
-        workflow.add_node("agent_action", ToolNode(self.tool_list))
-
-        workflow.add_node(
-            "generate", lambda state: try_except_continue(state, self.generate)
-        )  # Generating a response after we know the documents are relevant
-        # Call agent node to decide to retrieve or not
-        workflow.set_entry_point("agent")
-
-        workflow.add_edge('agent_action', 'agent')
-        # Decide whether to retrieve
+        
+        # Testing
+        workflow.add_node("objects", lambda state: try_except_continue(state, self.objects))
+        workflow.add_node("objects_action", ToolNode(self.tool_list))
+        workflow.add_node("object_plans", lambda state: try_except_continue(state, self.object_plans))
+        workflow.add_node("object_plans_action", ToolNode(self.tool_list))
+        workflow.add_node("order_plans", lambda state: try_except_continue(state, self.order))
+        
+        workflow.add_edge('objects_action', 'objects')
         workflow.add_conditional_edges(
-            "agent",
-            # Assess agent decision
-            from_agent_to,
+            "objects",
+            from_objects_to,
             {
-                # Translate the condition outputs to nodes in our graph
-                "agent_action": "agent_action",
-                "generate": "generate",
+                "objects_action": "objects_action",
+                "object_plans": "object_plans",
             },
         )
-        workflow.add_edge("generate", END)
+        workflow.add_edge('object_plans_action', 'object_plans')
+        workflow.add_conditional_edges(
+            "object_plans",
+            from_object_plans_to,
+            {
+                "object_plans_action": "object_plans_action",
+                "order_plans": "order_plans",
+            }
+        )
+        workflow.add_edge("order_plans", END)
+        workflow.set_entry_point("objects")
+        # # Define the nodes we will cycle between
+        # workflow.add_node("agent", lambda state: try_except_continue(state, self.agent))  # agent
+        # workflow.add_node("agent_action", ToolNode(self.tool_list))
+
+        # workflow.add_node(
+        #     "generate", lambda state: try_except_continue(state, self.generate)
+        # )  # Generating a response after we know the documents are relevant
+        # # Call agent node to decide to retrieve or not
+        # workflow.set_entry_point("agent")
+
+        # workflow.add_edge('agent_action', 'agent')
+        # # Decide whether to retrieve
+        # workflow.add_conditional_edges(
+        #     "agent",
+        #     # Assess agent decision
+        #     from_agent_to,
+        #     {
+        #         # Translate the condition outputs to nodes in our graph
+        #         "agent_action": "agent_action",
+        #         "generate": "generate",
+        #     },
+        # )
+        # workflow.add_edge("generate", END)
 
         # Compile
         self.graph = workflow.compile()
@@ -361,8 +576,8 @@ class ReMEmbRPlanner(Planner):
                                 (("user", question)),
             ]
         }
-
         out = self.graph.invoke(inputs)
+        # out = self.graph.invoke(inputs, config={"recursion_limit": 50})
         response = out['messages'][-1]
         response = ''.join(response.content.splitlines())
 
--- run_planner_offline.py
+++ run_planner_offline.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     parser.add_argument("--data_path", type=str, default=None)
     args = parser.parse_args()
     if args.data_path is None:
-        memory = remember_demo(observations=["The video starts with a shot of a kitchen counter with various cooking ingredients and utensils scattered around. The person's hand is seen reaching for a bottle of olive oil and a jar of spices. The camera then pans to a white cabinet where the person takes out a frying pan and places it on the stove. The person then opens the refrigerator and takes out a bag of cauliflower. The person places the cauliflower on the frying pan and begins to cook it. The camera focuses on the cauliflower as it cooks, with the person occasionally stirring it. The video ends with the person taking the cooked cauliflower out of the frying pan."])
+        # memory = remember_demo(observations=["The video starts with a shot of a kitchen counter with various cooking ingredients and utensils scattered around. The person's hand is seen reaching for a bottle of olive oil and a jar of spices. The camera then pans to a white cabinet where the person takes out a frying pan and places it on the stove. The person then opens the refrigerator and takes out a bag of cauliflower. The person places the cauliflower on the frying pan and begins to cook it. The camera focuses on the cauliflower as it cooks, with the person occasionally stirring it. The video ends with the person taking the cooked cauliflower out of the frying pan."])
+        memory = remember_demo()
         # memory = remember_demo(observations=["take out a frying pan from a white cabinet", "someone bring a califlower from refridge"])
     else:
         memory = remember_cobot(inpath=args.data_path)
